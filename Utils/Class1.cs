a9a50fc6c3ad253cccb52f4e1cfcad9c1dfa4d1a
Spencer Krause <thelostdreamer1@gmail.com>
2020-09-25 08:18:47 -0500

    Consensus and profile was not fun. But is done.

---
+++ Utils/Class1.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Utils
+{
+    public static class Class1
+    {
+        public static List<string> ProcessDNACollection(string DNACollection, int idLength)
+        {
+            List<string> processedCollection = DNACollection.Split('>').ToList();
+            processedCollection.RemoveAt(0);
+            for (int str = 0; str < processedCollection.Count; str++)
+            {
+                processedCollection[str] = processedCollection[str].Remove(0, idLength);
+                processedCollection[str] = processedCollection[str].Trim();
+                processedCollection[str] = processedCollection[str].Replace("\r\n", "");
+            }
+            return processedCollection;
+        }
+    }
+}
